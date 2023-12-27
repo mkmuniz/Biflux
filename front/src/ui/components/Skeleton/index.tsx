2f85be88419b1448f73c7076a72cbcb2c501901c
Mikael <mikaelmuniz2001@gmail.com>
2023-12-27 13:53:03 -0300

    fix: skeleton className

--- front/src/ui/components/Skeleton/index.tsx
+++ front/src/ui/components/Skeleton/index.tsx
@@ -5,13 +5,13 @@ export default function DashboardSkeleton() {
         <div className="p-4 sm:ml-64 bg-standard-dark">
             <div className="p-4">
                 <div className="grid grid-cols-2 gap-4 mb-4 items-center content-center h-screen">
-                    <div role="status" class="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
+                    <div role="status" className="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
                     </div>
-                    <div role="status" class="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
+                    <div role="status" className="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
                     </div>
-                    <div role="status" class="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
+                    <div role="status" className="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
                     </div>
-                    <div role="status" class="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
+                    <div role="status" className="flex items-center justify-center h-56 max-w-sm bg-blue-gray-400 rounded-lg animate-pulse">
                     </div>
                 </div>
             </div>
