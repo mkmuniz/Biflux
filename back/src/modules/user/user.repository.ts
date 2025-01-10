6c1389908f058eebd78e7f46017e9aab892aef26
Mkmuniz <mikael.muniz@bolha.com.br>
2025-01-10 20:16:46 -0300

    fix: error handling and refactor for user module

---
+++ back/src/modules/user/user.repository.ts
@@ -0,0 +1,38 @@
+import { PrismaClient, User } from "@prisma/client";
+import { UserDTO, UserEntity } from "../../types/user.types";
+
+export interface IUserRepository {
+    getAll(): Promise<User[]>;
+    findById(id: string): Promise<User | null>;
+    findByEmail(email: string): Promise<User | null>;
+    create(data: UserEntity): Promise<User>;
+    update(id: string, data: Partial<UserEntity>): Promise<User>;
+}
+
+export class UserRepository implements IUserRepository {
+    private db: PrismaClient;
+
+    constructor() {
+        this.db = new PrismaClient();
+    }
+
+    async getAll(): Promise<User[]> {
+        return this.db.user.findMany();
+    }
+
+    async findById(id: string): Promise<User | null> {
+        return this.db.user.findUnique({ where: { id } });
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.db.user.findUnique({ where: { email } });
+    }
+
+    async create(data: any): Promise<User> {
+        return this.db.user.create({ data });
+    }
+
+    async update(id: string, data: Partial<UserEntity>): Promise<User> {
+        return this.db.user.update({ where: { id }, data });
+    }
+}
--- back/src/modules/user/user.routes.ts
+++ back/src/modules/user/user.routes.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
+import { UserServices } from "./user.services";
 import { UserController } from "./user.controller";
+import { UserRepository } from "./user.repository";
+import { CloudStorageService } from "../cloudinary/cloudinary.services";
+
+
+const cloudStorageService = new CloudStorageService();
+const userRepository = new UserRepository();
+const userServices = new UserServices(userRepository, cloudStorageService);
+const userController = new UserController(userServices);
 
 const userRouter = Router();
 
 userRouter
-    .get('/user/:id', UserController.getUserById)
-    .post('/user', UserController.createUser)
-    .get('/user/profile/:id', UserController.getUserProfile)
-    .patch('/user/profile/:id', UserController.updateUserProfile)
-    
+    .get('/user/:id', (req, res) => userController.getUserById(req, res))
+    .post('/user', (req, res) => userController.createUser(req, res))
+    .get('/user/profile/:id', (req, res) => userController.getUserProfile(req, res))
+    .patch('/user/profile/:id', (req, res) => userController.updateUserProfile(req, res));
+
 export default userRouter;
