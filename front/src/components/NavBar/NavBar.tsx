e54326017fb8aabf7dd3e14d3e186ba878d92d42
Mkmuniz <mikael.muniz@bolha.com.br>
2025-01-11 03:00:08 -0300

    fix: navbar dropdown width

--- front/src/components/NavBar/NavBar.tsx
+++ front/src/components/NavBar/NavBar.tsx
@@ -37,7 +37,7 @@ export default function NavBar({ navLinks }: NavBar) {
     
     return (
         <nav className={`fixed w-full z-20 top-0 start-0 transition-all duration-300 ${scrolled ? 'bg-black backdrop-blur-lg' : 'bg-transparent'}`}>
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className={`${isOpen ? 'w-full' : 'max-w-screen-xl'} flex flex-wrap items-center justify-between mx-auto p-4`}>
                 <Link href="/" className="flex items-center">
                     <div className="relative">
                         <div className="absolute inset-0 bg-gradient-to-r from-[#8B5CF6] to-[#00A3FF] rounded-full blur-lg opacity-20"></div>
@@ -84,7 +84,7 @@ export default function NavBar({ navLinks }: NavBar) {
                     </button>
                 </div>
 
-                <div className={`items-center justify-between transition-all duration-500 ${isOpen ? 'mobile:h-screen bg-black/95 backdrop-blur-lg' : 'mobile:h-0'} w-full md:flex md:w-auto md:order-1`}>
+                <div className={`items-center justify-between transition-all duration-500 ${isOpen ? 'mobile:h-screen bg-black/95 backdrop-blur-lg fixed top-[72px] left-0 w-full' : 'mobile:h-0'} w-full md:flex md:w-auto md:order-1 md:relative md:top-0 md:bg-transparent`}>
                     <ul className={`flex flex-col text-center items-center justify-center p-4 md:p-0 font-medium md:space-x-8 md:flex-row h-full`}>
                         {navLinks.map((link: Link, index: Key) => (
                             <NavLink 
