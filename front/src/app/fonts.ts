5a932e881a6ec0e3fec34a4b5a4599c6cd0e22a7
Mkmuniz <mikael.muniz@bolha.com.br>
2025-01-13 11:25:17 -0300

    fix: build error

--- front/src/app/fonts.ts
+++ front/src/app/fonts.ts
@@ -1,14 +1,16 @@
 import { Roboto, Outfit } from 'next/font/google';
 
 export const roboto = Roboto({ 
-    weight: '700',
     subsets: ['latin'],
+    weight: ['400', '700'],
     display: 'swap',
-    variable: '--font-roboto'
+    variable: '--font-roboto',
+    preload: true,
 });
 
 export const outfit = Outfit({
     subsets: ['latin'],
     display: 'swap',
-    variable: '--font-outfit'
+    variable: '--font-outfit',
+    preload: true,
 }); 
