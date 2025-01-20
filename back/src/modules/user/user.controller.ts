b31663262edfdf202ae2e6ad203f4fc259cd1f8d
Mkmuniz <mikael.muniz@bolha.com.br>
2025-01-20 14:36:39 -0300

    refactor: controllers and error handle

--- back/src/modules/user/user.controller.ts
+++ back/src/modules/user/user.controller.ts
@@ -11,65 +11,66 @@ export class UserController {
     }
 
     async getUserById(req: Request, res: Response) {
-        try {
-            const { id } = req.params;
-            if (!id) return ErrorHandler.badRequest(res, "User ID is required");
+        const { id } = req.params;
+        if (!id) return ErrorHandler.badRequest(res, "User ID is required");
 
+        try {
             const user = await this.userServices.getUserById(id);
             if (!user) return ErrorHandler.notFound(res, "User not found");
 
             return res.status(200).json(user);
         } catch (err: any) {
             console.error(err.message);
-            return ErrorHandler.handleError(res);
+            return ErrorHandler.internalError(res);
         }
     }
 
     async createUser(req: Request, res: Response) {
-        try {
-            const userBody = req.body;
-            if (!userBody) return ErrorHandler.badRequest(res, "User fields are required");
+        const userBody = req.body;
+        if (!userBody) return ErrorHandler.badRequest(res, "User fields are required");
 
+        try {
             const user = await this.userServices.createUser(userBody);
             return res.status(201).json(user);
         } catch (err: any) {
             if (err.message === "User already exists") return ErrorHandler.conflict(res, err.message);
 
             console.error(err.message);
-            return ErrorHandler.handleError(res);
+            return ErrorHandler.internalError(res);
         }
     }
 
     async getUserProfile(req: Request, res: Response) {
-        try {
-            const { id } = req.params;
+        const { id } = req.params;
+        if (!id) return ErrorHandler.badRequest(res, "User ID is required");
+
+        try {    
             const user = await this.userServices.getUserById(id);
             if (!user) return ErrorHandler.notFound(res, "User not found");
 
             return res.status(200).json(user);
         } catch (err: any) {
             console.error(err.message);
-            return ErrorHandler.handleError(res);
+            return ErrorHandler.internalError(res);
         }
     }
 
     async updateUserProfile(req: Request, res: Response) {
+        const { id } = req.params;
+        const { name, email, profilePicture } = req.body;
+        if (!id) return ErrorHandler.badRequest(res, "User ID is required");
+
+        const isDataEmpty = !name && !email && !profilePicture;
+        if (isDataEmpty) return ErrorHandler.badRequest(res, "At least one field must be provided for update");
+
         try {
-            const { id } = req.params;
-            const { name, email, profilePicture } = req.body;
-
-            if (!id) return ErrorHandler.badRequest(res, "User ID is required");
-
-            const isDataEmpty = !name && !email && !profilePicture;
-            if (isDataEmpty) return ErrorHandler.badRequest(res, "At least one field must be provided for update");
-
             const userProfile = await this.userServices.updateUserProfile(id, { name, email, profilePicture});
             return res.status(200).json(userProfile);
         } catch (err: any) {
             if (err.message === 'User profile not found') return ErrorHandler.notFound(res, "User profile not found");
 
             console.error(err.message);
-            return ErrorHandler.handleError(res);
+            return ErrorHandler.internalError(res);
         }
     }
 }
